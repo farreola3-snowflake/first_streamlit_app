23cfeea89520712d7f661abd55f60772fc4974f7
farreola3-snowflake <125318270+farreola3-snowflake@users.noreply.github.com>
2023-02-26 18:50:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,6 @@ streamlit.dataframe(my_data_rows)
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+my_data_rows.append(add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit)
 
